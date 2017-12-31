2b2d5e31748777ca7df16479761c970910fc982f
Yoshihiro Tanaka <contact@cordea.jp>
2017-12-31 14:19:57 +0900

    add presenter

---
+++ app/src/main/java/jp/cordea/reiz/AddSessionPresenter.kt
@@ -0,0 +1,19 @@
+package jp.cordea.reiz
+
+import android.app.Activity
+import jp.cordea.reiz.databinding.ActivityAddSessionBinding
+
+class AddSessionPresenter(
+        override val activity: Activity,
+        override val binding: ActivityAddSessionBinding
+) : IPresenter<ActivityAddSessionBinding> {
+
+    override fun onCreate() {
+    }
+
+    override fun onResume() {
+    }
+
+    override fun onPause() {
+    }
+}
