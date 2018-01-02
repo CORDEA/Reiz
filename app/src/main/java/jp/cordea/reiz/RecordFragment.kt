1d8baadfb607e81fe047dd3709905457915494fd
Yoshihiro Tanaka <contact@cordea.jp>
2018-01-02 17:35:25 +0900

    set presenter

--- app/src/main/java/jp/cordea/reiz/RecordFragment.kt
+++ app/src/main/java/jp/cordea/reiz/RecordFragment.kt
@@ -5,19 +5,38 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import jp.cordea.reiz.databinding.FragmentRecordBinding
 
 class RecordFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private lateinit var binding: FragmentRecordBinding
+
+    private val presenter by lazy {
+        RecordPresenter(this, binding)
     }
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.fragment_record, container, false)
+    ): View =
+            FragmentRecordBinding.inflate(inflater, container, false).also {
+                binding = it
+            }.root
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        presenter.onCreate()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        presenter.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        presenter.onPause()
     }
 
     companion object {
